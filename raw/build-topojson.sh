dd4f8ed56c2d63f9b4278fb5d2b4eb52b4cfd468
Marc Löhe <marc@marcloehe.de>
2013-09-12 12:48:42 +0200

    Better map fitting, less data in build process

--- raw/build-topojson.sh
+++ raw/build-topojson.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# Build countries GeoJSON
+cd ne_10m_admin_0_countries
+rm countries.json
+./build-json.sh
+cd ..
+
+# Build states GeoJSON
+cd ne_10m_admin_1_states_provinces_shp
+rm states.json
+./build-json.sh
+cd ..
+
+# Build TopoJSON with countries and states
 topojson \
   --id-property su_a3 \
   -p name=NAME \
--- raw/ne_10m_admin_0_countries/build-json.sh
+++ raw/ne_10m_admin_0_countries/build-json.sh
@@ -2,6 +2,7 @@
 
 ogr2ogr \
 -f GeoJSON \
--where "adm0_a3 IN ('GBR', 'DEU', 'POL', 'DNK', 'NLD', 'CZE', 'FRA', 'AUT', 'CHE', 'LUX', 'BEL', 'LIE')" \
+-where "adm0_a3 IN ('GBR', 'POL', 'DNK', 'NLD', 'CZE', 'FRA', 'AUT', 'CHE', 'LUX', 'BEL', 'LIE', 'SVK', 'SVN', 'HUN', 'ITA', 'HRV', 'BIH')" \
+-clipdst -5 44 20 56 \
 countries.json \
 ne_10m_admin_0_countries.shp
