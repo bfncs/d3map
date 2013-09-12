d8dc7df6c5f19f0d0e0ed235cff76d8b96edb2e1
Marc Löhe <marc@marcloehe.de>
2013-09-12 12:19:30 +0200

    Clean unused files

--- raw/build-topojson.bak.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-topojson \
-  --id-property su_a3 \
-  -p name=NAME \
-  -p name \
-  -o nlgeo.json \
-  ne_10m_admin_0_countries/countries.json \
-  ne_10m_populated_places/places.json
